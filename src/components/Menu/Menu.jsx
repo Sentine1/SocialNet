2c5a657f971c8647b31dce4338209c4d7329425c
Антон Забабурин <asumosan@gmail.com>
2024-10-09 17:31:32 +0500

    Add NavLink and some css change fight vs activeClassName

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import module from './Menu.module.css'
+import {NavLink} from "react-router-dom";
+import "./Menu.css";
+
 
 function Menu() {
 return <nav className={module.Menu}>
 
 	<div className={module.Items}>
-		<a href="/profile">Profile</a>
+		<NavLink to="/profile">Profile</NavLink>
 	</div>
 	<div className={module.Items}>
-		<a href="/news">News</a>
+		<NavLink to="/news">News</NavLink>
 	</div>
 	<div className={module.Items}>
-		<a href="/friends">Friends</a>
+		<NavLink to="/friends">Friends</NavLink>
 	</div>
 	<div className={module.Items}>
-		<a href="/videos">Videos</a>
+		<NavLink to="/videos">Videos</NavLink>
 	</div>
 	<div className={module.Items}>
-		<a href="/audios">Audios</a>
+		<NavLink to="/audios">Audios</NavLink>
 	</div>
 	<div className={module.Items}>
-		<a href="/dialogs">Messages</a>
+		<NavLink to="/dialogs">Messages</NavLink>
 	</div>
 	<br/>
 	<br/>
 	<div className={`${module.Items} ${module.Active}`}>
-		<a href="/settings">Settings</a>
+		<NavLink to="/settings">Settings</NavLink>
 	</div>
 </nav>
 };
