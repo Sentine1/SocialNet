582c23a19a32704af6423b33fd51821dea2b9aec
Антон Забабурин <asumosan@gmail.com>
2024-11-26 15:43:01 +0500

    Self Post Container  lesson 43

---
+++ src/components/Content/SelfPosts/SelfPostsContainer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import module from './SelfPosts.module.css'
+import Post from "./Post/Post";
+import {addPostActionCreator, onChangeActionCreator} from "../../../redux/ProfileReducer";
+
+function SelfPosts(props) {
+    let newPostElement = React.createRef();
+    let addPost = () =>{
+        props.dispatch(addPostActionCreator())
+    }
+    
+    let onChangeEvent = (e) => {
+        let text =e.target.value;
+        let action = onChangeActionCreator(text);
+        props.dispatch(action)
+    }
+
+    return <div className={module.Content}>
+        <div>
+            <div>
+                <textarea onChange={onChangeEvent} ref={newPostElement}
+                          value={props.newPostText} placeholder="New post"/>
+                <div/>
+                <div>
+                    <button onClick={addPost}>Save</button>
+                    <button onClick={() => {
+                        alert('Damn!!')
+                    }}>Delete
+                    </button>
+                </div>
+                {props.PostMessage.map(e => (<Post message={e.message} like={e.like}/>))}
+            </div>
+        </div>
+    </div>
+}
+
+export default SelfPosts;
